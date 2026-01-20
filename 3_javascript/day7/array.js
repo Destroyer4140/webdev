5f99eab65039e8961dcbc5da65c5419593d9bca6
Shubham Kumar <65092578+Destroyer4140@users.noreply.github.com>
2026-01-20 21:08:01 +0530

    Code Commit

---
+++ 3_javascript/day7/array.js
@@ -0,0 +1,26 @@
+let num = [1, 2, 3, "Ram", "Shubham"]
+// console.log(num)
+// console.log(typeof(num))
+// console.log(num.length);
+
+// console.log(typeof(num[3]));
+
+// num.push(true);
+// console.log(num)
+
+// console.log(num.pop());
+
+let arr = [20, 30, 50 , 60, 70, 90, 110]
+// for(let i=0; i<arr.length; i++) {
+//     console.log(arr[i])
+// }
+
+// for(let num of arr) {
+//     console.log(num)
+// }
+
+//Slicing in array
+
+console.log(arr.slice(2,4))
+console.log(arr.splice(1, 3))
+console.log(arr)
