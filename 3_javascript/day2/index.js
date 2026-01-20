f8ead27f91147d9845a33da19ad2d67a70425e6a
Shubham Kumar <65092578+Destroyer4140@users.noreply.github.com>
2026-01-20 21:08:43 +0530

    Code Commit

---
+++ 3_javascript/day2/index.js
@@ -0,0 +1,10 @@
+let name = "Rohit"
+let age = 20
+
+console.log(name, age)
+
+const account = 1234
+console.log(account)
+
+// account = 4352
+console.log(account)
---
+++ 3_javascript/day3/string.js
@@ -0,0 +1,52 @@
+// const str = `Hello Shubham Kumar.
+// I am an Engineer. Hello`;
+
+// console.log(str)
+// console.log(str.length)
+
+// console.log(str[0])
+// to upper case
+// console.log(str.toUpperCase())
+
+// to lower case
+// console.log(str.toLowerCase())
+
+// console.log(str.indexOf('Kum'))
+
+// console.log(str.lastIndexOf('llo'))
+
+// console.log(str.includes('llo'))
+
+// console.log(str.slice(0, 5))
+
+// console.log(str.slice(-6, -2))
+
+// console.log(str.replaceAll('llo', 'iam'))
+
+// const uname = " Shubham Kumar   ";
+// console.log(uname.trim())
+
+// console.log(uname.trim().split(' '))
+
+// Date
+// const now = new Date();
+// console.log(now)
+// console.log(now.toString());
+
+// console.log(now.toISOString());
+
+// console.log(now.toLocaleString());
+
+
+// Local time
+// console.log(now.getDay());
+// console.log(now.getDate());
+// console.log(now.getMonth());
+// console.log(now.getFullYear());
+
+
+const now = new Date(2025,8,20,12,45,20,456);
+console.log(now.toString());
+
+const curr = Date.now();
+console.log(curr.toString())
